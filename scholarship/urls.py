8baf443e0673cf04317e9cbc7518947920b08279
VincentGichohi <vincentegichohi@gmail.com>
2022-05-20 16:12:44 +0300

    add urls to the apps

--- scholarship/urls.py
+++ scholarship/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('student/', include('student.urls')),
     path('sponsor/', include('sponsor.urls')),
-    path('validation/', include('validation.urls'))
+    path('validation/', include('validation.urls')),
 ]
