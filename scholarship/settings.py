f678ba7bc7c4acf0f83fd14a264d3a3c5211f943
VincentGichohi <vincentegichohi@gmail.com>
2022-05-20 16:11:02 +0300

    Modify the user model

--- scholarship/settings.py
+++ scholarship/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+# from validation.models import MyUser, User
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,3 +135,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "validation.MyUser"
