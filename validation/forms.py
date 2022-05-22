b180a3547fe0337328ecc539a0cf5901178fd0fb
VincentGichohi <vincentegichohi@gmail.com>
2022-05-22 12:28:02 +0300

    Add a form for creating users

--- validation/forms.py
+++ validation/forms.py
@@ -39,3 +39,38 @@ class RegisterForm(forms.ModelForm):
             self.add_error('password2', 'Your passwords must match')
         return cleaned_data
 
+
+class UserAdminCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required fields plus a 
+    repeated password
+    """
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = MyUser
+        fields = ['email']
+
+
+    def clean(self):
+        """
+        Verify both passwords match.
+        """
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_2 = cleaned_data.get('password2')
+        if password is not None and password != password_2:
+            self.add_error('password_2', 'Your passwords must match')
+        return cleaned_data
+
+    def save(self, commit=True):
+        #Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+        
+
+
