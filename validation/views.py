f3832b628dd7743d5c4e14f03a48eebd6411f2b5
VincentGichohi <vincentegichohi@gmail.com>
2022-07-24 20:53:51 +0300

    Add imports to the registration views

--- validation/views.py
+++ validation/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 
-# Create your views here.
+
+# class SignUpView(SuccessMessageMixin, CreateView):
+#     template_name = ''
