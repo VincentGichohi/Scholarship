4b19a6d65496404728930689368d76adb5438f7e
VincentGichohi <vincentegichohi@gmail.com>
2022-07-24 21:34:16 +0300

    Modify the views class

--- validation/views.py
+++ validation/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .forms import RegisterForm
 from django.views.generic import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 
 
-# class SignUpView(SuccessMessageMixin, CreateView):
-#     template_name = ''
+class SignUpView(SuccessMessageMixin, CreateView):
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+    form_class = RegisterForm
+    success_message = "Your user account has been created successfully"
