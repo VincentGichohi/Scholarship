62aa4ea9effbb119954987b6a5243efe5a52389d
VincentGichohi <vincentegichohi@gmail.com>
2023-02-21 22:59:44 +0300

    edit customusermodel

--- validation/models.py
+++ validation/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
+# from PIL import Image
 
-class UserManager(BaseUserManager):
+
+class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """
         Creates and saves a User with the given email and password.
@@ -45,52 +47,44 @@ class UserManager(BaseUserManager):
         return user
 
 
-class MyUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
-    is_student = models.BooleanField(default=False)
-    is_sponsor = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False) # a admin user; non super-user
-    admin = models.BooleanField(default=False) # a superuser
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE = ((1, "Admin"), (2, "Staff"), (3, "Client"))
+    GENDER = [("M", "Male"), ("F", "Female")]
 
-    # notice the absence of a "Password field", that is built in.
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(default=1, choices=USER_TYPE, max_length=1)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    password2 = models.CharField(default='', max_length=100)
+    profile_pic = models.ImageField()
+    is_staff = models.BooleanField(default=False)
+    address = models.TextField()
+    fcm_token = models.TextField(default="")  # For firebase notifications
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] # Email & Password are required by default.
-
-    def get_full_name(self):
-        # The user is identified by their email address
-        return self.email
-
-    def get_short_name(self):
-        # The user is identified by their email address
-        return self.email
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
 
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
 
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
+class Profile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    avatar = models.ImageField(default="", upload_to="profile_images")
 
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        return self.staff
+    def __str__(self):
+        return self.user.email
 
-    @property
-    def is_admin(self):
-        "Is the user a admin member?"
-        return self.admin
+    # def save(self, *args, **kwargs):
+    #     super().save()
+    #
+    #     img = Image.open(self.avatar.path)
+    #
+    #     if img.height > 100 or img.width > 100:
+    #         new_img = (100, 100)
+    #         img.thumbnail(new_img)
+    #         img.save(self.avatar.path)
