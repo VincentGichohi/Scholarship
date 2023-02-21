bd8c763dc97b3213a589b2454283a46bc5b1857b
VincentGichohi <vincentegichohi@gmail.com>
2023-02-22 01:17:10 +0300

    add a register serializer with a validate method

--- validation/serializers.py
+++ validation/serializers.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
 from .models import CustomUser, Profile
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'password', 'password2']
+        extra_kwargs = {'password': {'write_only': True},
+                        'password2': {'write_only': True}}
+
+        def validate(self, data):
+            try:
+                validate_email(data['email'])
+            except ValidationError:
+                raise serializers.ValidationError("Invalid Email Format")
+            if len(data['password']) < 8:
+                raise serializers.ValidationError("Passwords must be at least 8 characters")
+            if data['password'] != data['password2']:
+                raise serializers.ValidationError("Passwords do not match")
+
+            if CustomUser.objects.filter(email=data['email']).exists():
+                raise serializers.ValidationError('User with given email already exists')
+            return data
 
