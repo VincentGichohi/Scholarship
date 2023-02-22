abccf9a24880a50ddf600b6c7d6b50274537e775
VincentGichohi <vincentegichohi@gmail.com>
2023-02-22 15:28:31 +0300

    add imports to validation views

--- validation/views.py
+++ validation/views.py
@@ -1,12 +1,26 @@
+from django.contrib.auth import authenticate, get_user_model, login
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-# from .forms import RegisterForm
-from django.views.generic import CreateView
-from django.contrib.messages.views import SuccessMessageMixin
+from rest_framework import views
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .models import CustomUser
+from rest_framework import generics
+from knox.models import AuthToken
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+# from knox.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from rest_framework import generics, permissions, serializers
+from rest_framework import status
+from django.template.response import TemplateResponse
+from knox.auth import TokenAuthentication
+from rest_framework.decorators import api_view, permission_classes
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from rest_framework.views import APIView
+# from rest_framework_social_oauth2.authentication import SocialAuthentication
+from social_django.utils import psa
 
+User = get_user_model()
 
-class SignUpView(SuccessMessageMixin, CreateView):
-    template_name = 'register.html'
-    success_url = reverse_lazy('login')
-    # form_class = RegisterForm
-    success_message = "Your user account has been created successfully"
