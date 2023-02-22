9a8b0ca059655bec0d6a0e9657a70d794737fc53
VincentGichohi <vincentegichohi@gmail.com>
2023-02-22 21:37:54 +0300

    add hosst user and password

--- scholarship/settings.py
+++ scholarship/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True   # or False, depending on your SMTP server configuration
-EMAIL_HOST_USER = 'your_smtp_username'
-EMAIL_HOST_PASSWORD = 'your_smtp_password'
+EMAIL_HOST_USER = 'vinnygee857@gmail.com'
+EMAIL_HOST_PASSWORD = 'P@55w0rd!###'
 
 
 # Static files (CSS, JavaScript, Images)
