9d8def32556d84ceaba92436933654dd4736d7fe
VincentGichohi <vincentegichohi@gmail.com>
2023-02-22 21:20:20 +0300

    add a status to student model

--- student/models.py
+++ student/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from validation.models import CustomUser
 
 
+SPONSORSHIP_STATUS = [
+    ('Pending', 'PENDING'),
+    ('Approved', 'APPROVED'),
+    ('Rejected', 'REJECTED')
+]
+
+
 class Student(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=100)
+    status = models.CharField(max_length=2, choices=SPONSORSHIP_STATUS)
     phone = models.IntegerField(max_length=10)
     email = models.EmailField(max_length=255, null=False, blank=False)
     birth_cert_file = models.FileField(upload_to='files')
