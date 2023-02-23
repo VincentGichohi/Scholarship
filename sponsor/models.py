78c815945611b18a54fed03d6ab736c6518a9557
VincentGichohi <vincentegichohi@gmail.com>
2023-02-23 12:05:36 +0300

    add a staff model

--- sponsor/models.py
+++ sponsor/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from validation.models import CustomUser
 
+
+class Staff(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user.email
