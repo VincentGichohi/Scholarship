abbdfc381e42cfbd57a981d5ee256aef4861edff
VincentGichohi <vincentegichohi@gmail.com>
2023-02-23 12:05:51 +0300

    add a sponsor model

--- sponsor/models.py
+++ sponsor/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from validation.models import CustomUser
 
 
-class Staff(models.Model):
+class Sponsor(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
 
